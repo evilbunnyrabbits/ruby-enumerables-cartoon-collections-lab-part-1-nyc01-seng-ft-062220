c391732ac28735b9610d85ed7ddd3b72d1f5d4eb
beaming-subroutine-5164 <ben.garlock@gmail.com>
2020-06-01 12:27:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,9 @@
 def greet_characters(array)
-  # Use `each` to enumerate over the provided array
-  #
-  # Print a custom greeting for each element
+  array.each do |item|
+     puts "Hello #{item}"
+  end
 end
+
 
 def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
