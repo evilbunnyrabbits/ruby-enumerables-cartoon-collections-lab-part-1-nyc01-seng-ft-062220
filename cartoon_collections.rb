52da5df6cc780707d068d2f7fe86a952875f430f
beaming-subroutine-5164 <ben.garlock@gmail.com>
2020-06-01 12:34:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,9 +4,11 @@ def greet_characters(array)
   end
 end
 
-
 def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
+  array.each_with_index do |value, index| value
+  puts "#{index +1}. #{value}"
+  end
 end
